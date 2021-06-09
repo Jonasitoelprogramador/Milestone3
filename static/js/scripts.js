51111fa7b238c871f0c71dc466da2ee1f61ef861
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-06-09 14:46:45 +0000

    add javascript code in order to create a new input for each ingredient in order to send the input back to mongo in array form

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,9 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.0 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var next_ingredient = document.getElementById("next-ingredient");
+next_ingredient.addEventListener("click", next_input);
+
+function next_input() {
+    var inpt = document.createElement("input");
+    inpt.className("btn btn-primary");
+    var element = document.getElementsByClassName("ingredients");
+    element.appendChild(inpt);
+}

a5187ade5677c52b48396af3e7b413c36bcb4e72
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-06-10 14:45:11 +0000

    create function that sets the innerhtml for each new bootstrap row that is created

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,27 @@
-var next_ingredient = document.getElementById("next-ingredient");
-next_ingredient.addEventListener("click", next_input);
+function event_listener() {
+    var next_ingredient = document.getElementsByClassName("next-ingredient");
+    next_ingredient[len(next_ingredient) - 1]
+    next_ingredient.addEventListener("click", next_input);
+}
 
 function next_input() {
     document.getElementsByClassName("ingredients")[0].lastElementChild.readOnly = true;
-    var inpt = document.createElement("input");
-    inpt.className = "form-control input-margin";
+    var inpt = document.createElement("div");
+    inpt.className = "row";
     inpt.setAttribute = ("type", "text");
+    inpt.innerHTML = 
+    `<div class="col-md-6">
+            <input type="text" class="form-control" id="exampleFormControlInput3" name="ingredients" placeholder="Kosovan">
+    </div>
+    <div class="col-md-6"> 
+            <input type="button" class="btn btn-primary next-ingredient-class" value="Next Ingredient" id="next-ingredient">
+            <input type="button" class="btn btn-primary" value="Done" id="done">
+            <input type="button" class="btn btn-primary" value="Back" id="undo-input">
+    </div>`;
     var element = document.getElementsByClassName("ingredients")[0];
     console.log(element);
     element.appendChild(inpt);
+    event_listener()
 }
 
 var next_ingredient = document.getElementById("done");

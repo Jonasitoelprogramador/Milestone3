5de4ef6a000fa92abdf63f21d215c1f9502193b6
Jonasitoelprogramador <jonahparrylloyd@gmail.com>
2021-06-11 07:34:07 +0000

    edit event_listener and next_input functions to fix bug

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,18 @@
 function event_listener() {
     var next_ingredient = document.getElementsByClassName("next-ingredient");
-    next_ingredient[len(next_ingredient) - 1]
-    next_ingredient.addEventListener("click", next_input);
-}
+    console.log(next_ingredient.length);
+    the_next_ingredient = next_ingredient[next_ingredient.length - 1];
+    console.log(the_next_ingredient);
+    the_next_ingredient.addEventListener("click", next_input);}
+
+
+event_listener()
+
 
 function next_input() {
-    document.getElementsByClassName("ingredients")[0].lastElementChild.readOnly = true;
+    console.log("hi")
+    row = document.getElementsByClassName("ingredients")[0].lastElementChild;
+    row.children[0].children[0].readOnly = true;
     var inpt = document.createElement("div");
     inpt.className = "row";
     inpt.setAttribute = ("type", "text");
@@ -14,7 +21,7 @@ function next_input() {
             <input type="text" class="form-control" id="exampleFormControlInput3" name="ingredients" placeholder="Kosovan">
     </div>
     <div class="col-md-6"> 
-            <input type="button" class="btn btn-primary next-ingredient-class" value="Next Ingredient" id="next-ingredient">
+            <input type="button" class="btn btn-primary next-ingredient" value="Next Ingredient">
             <input type="button" class="btn btn-primary" value="Done" id="done">
             <input type="button" class="btn btn-primary" value="Back" id="undo-input">
     </div>`;
